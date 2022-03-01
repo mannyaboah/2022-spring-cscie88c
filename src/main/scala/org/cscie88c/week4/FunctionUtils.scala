885f0d663eddf9c110d3acb68a3e4dd628f83037
mannyaboah <ema526@g.harvard.edu>
2022-02-28 22:55:59 -0500

    Week4 solution

--- src/main/scala/org/cscie88c/week4/FunctionUtils.scala
+++ src/main/scala/org/cscie88c/week4/FunctionUtils.scala
@@ -1,11 +1,30 @@
 package org.cscie88c.week4
 
 object FunctionUtils {
-  
+
   // complete the implementation of the higher order functions below
-  def applyNtimes(n: Int)(x: Int)(f: Int => Int): Int = ???
 
-  def myPositivePower(x: Int, n: Int): Int = ???
+  def add5(x: Int) = x + 5
 
-  def deferredExecutor(name: String)(f: Int => Int): Int => Int = ???
+  def applyNtimes(n: Int)(x: Int)(f: Int => Int): Int =
+    if (n == 1) f(x) else f(x) + applyNtimes(n - 1)(x)(f)
+
+  def applyNtimesExp(n: Int)(x: Int)(f: Int => Int): Int =
+    if (n == 1) f(x) else if (n <= 0) 1 else f(x) * applyNtimes(n - 1)(x)(f)
+
+  // Questionable
+  def applyNtimesFold(n: Int)(x: Int)(f: Int => Int): Int =
+    List.fill(n)(x).foldLeft(0)(f(_) + f(_))
+
+  def myPositivePower(x: Int, n: Int): Int =
+    applyNtimesExp(n)(x)((x: Int) => x + 0)
+
+  def deferredExecutor(
+      name: String
+    )(
+      f: Int => Int
+    ): Int => Int = { (x: Int) =>
+    println(s"running on deferred executor ${name} with value ${x}")
+    f(x)
+  }
 }
--- src/test/scala/org/cscie88c/week4/FunctionUtilsTest.scala
+++ src/test/scala/org/cscie88c/week4/FunctionUtilsTest.scala
@@ -1,18 +1,37 @@
 package org.cscie88c.week4
 
-import org.cscie88c.testutils.{StandardTest}
+import org.cscie88c.testutils.{ StandardTest }
 
 class FunctionUtilsTest extends StandardTest {
 
-   "FunctionUtils" when {
+  "FunctionUtils" when {
     "calling applyNtimes" should {
-      "return the correct value" in {
-        // write unit test here
+      "return the correct values for applying some functon N times 😅" in {
+        val testI = (1, 2)
+        val testII = (2, 2)
+        val testIII = (3, 0)
+        val testIV = (4, 2)
+        val testV = (5, 0)
+
+        def add2(x: Int): Int = x + 2
+
+        FunctionUtils.applyNtimes(testI._1)(testI._2)(add2) shouldBe 4
+        FunctionUtils.applyNtimes(testII._1)(testII._2)(add2) shouldBe 8
+        FunctionUtils.applyNtimes(testIII._1)(testIII._2)(add2) shouldBe 6
+        FunctionUtils.applyNtimes(testIV._1)(testIV._2)(add2) shouldBe 16
+        FunctionUtils.applyNtimes(testV._1)(testV._2)(add2) shouldBe 10
       }
-    
+
     }
 
-    // write unit tests for other functions here
+    "calling defered executer should defer 😄" should {
+      "deffer and add 5" in {
+        val test = FunctionUtils.deferredExecutor("test")((x: Int) => x + 5)
+        val res = test(5)
+
+        res shouldBe 10
+      }
+    }
   }
- 
+
 }
