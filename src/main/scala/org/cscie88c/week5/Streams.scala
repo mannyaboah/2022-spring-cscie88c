9ca6684b096a9040255d54de99ffd4d14435389f
mannyaboah <ema526@g.harvard.edu>
2022-03-03 12:13:39 -0500

    hw5 solution

--- src/main/scala/org/cscie88c/week5/Streams.scala
+++ src/main/scala/org/cscie88c/week5/Streams.scala
@@ -7,17 +7,28 @@ object Streams {
   val rnd = new Random()
   def uuid: String = UUID.randomUUID.toString.replaceAll("-", "")
 
-  case class Dog(name: String, age: Int, hasCurrentShots: Boolean)
+  case class Dog(
+      name: String,
+      age: Int,
+      hasCurrentShots: Boolean
+    )
 
-  val mult5: LazyList[Int] = ???
+  val mult5: LazyList[Int] = LazyList.range(0, 100).filter(_ % 5 == 0)
 
-  val randIntStream: LazyList[Int] = ???
+  val randIntStream: LazyList[Int] = LazyList.continually(rnd.between(0, 15))
 
-  val dogs: LazyList[Dog] = ???
+  val dogs: LazyList[Dog] = LazyList.continually(
+    Dog(s"dog-${uuid}", rnd.between(0, 15), rnd.nextBoolean())
+  )
 
-  def healthyDogs(dogs: LazyList[Dog]): LazyList[Dog] = ???
+  def healthyDogs(dogs: LazyList[Dog]): LazyList[Dog] =
+    dogs.filter(_.hasCurrentShots == true)
 
-  def averageHealthyAge(allDogs: LazyList[Dog], sampleSize: Int): Double = ???
-
+  def averageHealthyAge(allDogs: LazyList[Dog], sampleSize: Int): Double =
+    healthyDogs(allDogs)
+      .take(sampleSize)
+      .map(_.age)
+      .reduceLeft(_ + _)
+      .toDouble / sampleSize.toDouble
 
 }
