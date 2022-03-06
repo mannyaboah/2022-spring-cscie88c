4892988d44f9700a426ffa46366809966f18193d
mannyaboah <ema526@g.harvard.edu>
2022-03-06 17:19:50 -0500

    Week2 solutions

--- src/test/scala/org/cscie88c/week2/AdministratorTest.scala
+++ src/test/scala/org/cscie88c/week2/AdministratorTest.scala
@@ -1,8 +1,26 @@
 package org.cscie88c.week2
 
-import org.cscie88c.testutils.{StandardTest}
+import org.scalatest.funsuite.AnyFunSuite
 
 // write unit tests for Administrator below
-class AdministratorTest extends StandardTest {
-  
+class AdministratorTest extends AnyFunSuite {
+
+  var happyAdmin =
+    new Administrator("Professor X", "X@xmenacademy.org", 5000000)
+  var sadAdmin = new Administrator("Professor X", "", 50L)
+
+  test("😄 Should contain Name, Email, and Budget") {
+    val test =
+      s"Name: ${happyAdmin.name}, Email: ${happyAdmin.email}, Budget: $$${happyAdmin.budget}"
+    val description = happyAdmin.description
+    assert(description.contains(test))
+  }
+
+  test("☹️ Should not contain Name, Email, and Budget") {
+    val test =
+      s"Name: ${happyAdmin.name}, Email: ${happyAdmin.email}, Budget: $$${happyAdmin.budget}"
+    val description = sadAdmin.description
+    assert(!description.contains(test))
+  }
+
 }
--- src/test/scala/org/cscie88c/week2/SubjectTest.scala
+++ src/test/scala/org/cscie88c/week2/SubjectTest.scala
@@ -1,9 +1,24 @@
 package org.cscie88c.week2
 
-import org.cscie88c.testutils.{StandardTest}
+import org.scalatest.wordspec.AnyWordSpec
 
 // write unit tests for Subject below
 
-class SubjectTest extends StandardTest {
-  
+class SubjectTest extends AnyWordSpec {
+  "Subject Constructor and Functions" when {
+    val testData: List[Subject] = List(
+      "1,Physical Education,true",
+      "2,Chemistry,false"
+    ).map(Subject(_))
+
+    "Executed" should {
+      "Create two subjects with respective attributes" in {
+        val subject1 = "Physical Education"
+        val subject2 = "Chemistry"
+
+        assert(testData.size == 2)
+        assert(testData(0).name == subject1 && testData(1).name == subject2)
+      }
+    }
+  }
 }
