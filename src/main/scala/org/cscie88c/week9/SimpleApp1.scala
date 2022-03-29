aeace593b8df9c5d2e09bd64d955d1b7bbd1fa58
mannyaboah <ema526@g.harvard.edu>
2022-03-29 13:38:29 -0400

    initial solution commit

--- src/main/scala/org/cscie88c/week9/SimpleApp1.scala
+++ src/main/scala/org/cscie88c/week9/SimpleApp1.scala
@@ -1,19 +1,21 @@
 package org.cscie88c.week9
 
 import com.typesafe.scalalogging.LazyLogging
-import scala.util.{Try, Success, Failure}
+
 import scala.io.Source
+import scala.util.Try
 
 // run using: sbt "runMain org.cscie88c.week9.SimpleApp1 <args>"
 object SimpleApp1 extends LazyLogging {
-  
+
   def lineStreamFromFile(fileName: String): Option[LazyList[String]] =
     Try {
       LazyList.from(Source.fromResource(fileName).getLines())
     }.toOption
-  
-  def monthLines(month: String)(lines: LazyList[String]): LazyList[String] = ???
-      
-  def main(args: Array[String]): Unit = ???
+
+  def monthLines(month: String)(lines: LazyList[String]): LazyList[String] =
+    lines.filter(_.contains(month))
+
+  def main(args: Array[String]): Unit = {}
 
 }
