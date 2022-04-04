210c0a66b364896a365db16dbc358b3123102286
mannyaboah <ema526@g.harvard.edu>
2022-04-04 17:43:38 -0400

    Week9 solution

--- src/main/scala/org/cscie88c/MainApp.scala
+++ src/main/scala/org/cscie88c/MainApp.scala
@@ -1,24 +1,36 @@
-/**
-  * main entry point for application
+/** main entry point for application
   */
 
 package org.cscie88c
 
-import org.cscie88c.config.{ConfigUtils, CookieSettings}
+import org.cscie88c.config.{ ConfigUtils, CookieSettings, SimpleApp2Config }
 import pureconfig.generic.auto._
-import com.typesafe.scalalogging.{LazyLogging}
+import com.typesafe.scalalogging.{ LazyLogging }
 
 object MainApp extends LazyLogging {
 
-  val COOKIE_CONFIG_PATH="org.cscie88c.cookie"
+  val COOKIE_CONFIG_PATH = "org.cscie88c.cookie"
+  val SIMPLE_APP_CONF_PATH = "org.cscie88c.simpleApp2"
 
-  def hello(name: String)(implicit conf: CookieSettings): String = {
-    logger.info(s"running application version with ttl: ${conf.ttl}")
+  def hello(
+      name: String
+    )(implicit
+      conf: CookieSettings,
+      simpleAppConf: SimpleApp2Config
+    ): String = {
+    logger.info(
+      s"running application version with ttl: ${conf.ttl}. \nApp properties: " +
+        s"fileName: ${simpleAppConf.filename}"
+    )
     s"Hello ${name}"
   }
 
   def main(args: Array[String]): Unit = {
-    implicit val cookie = ConfigUtils.loadAppConfig[CookieSettings](COOKIE_CONFIG_PATH)
+    implicit val cookie =
+      ConfigUtils.loadAppConfig[CookieSettings](COOKIE_CONFIG_PATH)
+
+    implicit val simpleApp2 =
+      ConfigUtils.loadAppConfig[SimpleApp2Config](SIMPLE_APP_CONF_PATH)
 
     val name = args.length match {
       case 0 => "Anonymous"
@@ -28,4 +40,3 @@ object MainApp extends LazyLogging {
     println(message)
   }
 }
-
--- src/main/scala/org/cscie88c/week9/SimpleApp1.scala
+++ src/main/scala/org/cscie88c/week9/SimpleApp1.scala
@@ -3,6 +3,8 @@ package org.cscie88c.week9
 import com.typesafe.scalalogging.LazyLogging
 
 import scala.io.Source
+import scala.util.Failure
+import scala.util.Success
 import scala.util.Try
 
 // run using: sbt "runMain org.cscie88c.week9.SimpleApp1 <args>"
@@ -16,6 +18,15 @@ object SimpleApp1 extends LazyLogging {
   def monthLines(month: String)(lines: LazyList[String]): LazyList[String] =
     lines.filter(_.contains(month))
 
-  def main(args: Array[String]): Unit = {}
+  def janLength(fileName: String): String = Try {
+    lineStreamFromFile(fileName)
+  }.map(lines => monthLines("Jan")(lines.get).size) match {
+    case Failure(exception) =>
+      s"No valid transactions found for Jan. \n${exception}"
+    case Success(value) => value.toString()
+  }
+
+  def main(args: Array[String]): Unit =
+    println(janLength(args(0)))
 
 }
